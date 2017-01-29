eff02e9fbe494cb7a372c8a62b39da8a46c46646
Joshua Mabry <inkandcapture@gmail.com>
2017-01-29 08:09:54 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule,
