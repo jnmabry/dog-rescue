6b349a84403beb40e6c06cb5d38d465ccd6b9546
Joshua Mabry <inkandcapture@gmail.com>
2017-01-29 08:26:52 -0500

    Added Navigation and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { NavComponent } from './home-screen/nav/nav.component';
+import { FooterComponent } from './home-screen/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeScreenComponent
+    HomeScreenComponent,
+    NavComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
